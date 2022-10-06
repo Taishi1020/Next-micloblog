a480b3caf869b60f12d3b58f54e458797e46bf06
Taishi1020 <tai.yakiman1020@gmail.com>
2022-10-06 11:19:36 +0900

    upd:　マークダウンファイルを解析するライブラリを追加と記述

---
+++ lidev/post.js
@@ -0,0 +1,25 @@
+import path from "path"
+import fs from "fs"
+import matter from "matter";
+
+const postDirectory = path.join(process.cwd(), "posts")
+
+//mdファイルのデータを取り出す
+export function getPostData() {
+    const fileNames = fs.readdirSync(postDirectory);
+    const allPostsData = fileNames.map((filename) => {
+        const id = filename.replace(/¥.md$/, ""); //ファイル名
+
+        //マークダウンファイルを文字列として読み取る
+        const fullPath = path.join(postDirectory, filename);
+        const fileContents = fs.readFileSync(fullPath, "utf8")
+
+        const matterResult = matter(fileContents);
+
+        //idとデータを返す.
+        return{
+            id,
+            ...matterResult    //matterResultはマークダウンファイルに記述されているtitle: date: thumbnailが格納されている
+        }
+    });
+}
